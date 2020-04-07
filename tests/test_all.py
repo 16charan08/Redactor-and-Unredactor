34c8ab66b9f9461dc579d6aac6836aa011b4b2a6
Ramcharan Reddy.K <ramcharankankanala@gmail.com>
2020-04-06 21:27:49 -0500

    Update test_all.py

--- tests/test_all.py
+++ tests/test_all.py
@@ -15,25 +15,16 @@ def test_names():
     Read_data = []
     all = []
     files = ["*.txt","*.md"]
-    #print(files)
     data = glob.glob(str(files))
     for i in data:
         Read_data.append(open(i).read())
-   # print(Read_data)
     masked_names = p1.names(Read_data)
-    #print(masked_names)
-
     for i in masked_names:
         wordlist = word_tokenize(i)
-       # print(wordlist)
-       # print(i)
         for j in wordlist:
-            #print(j)
             x = u'\u2588' * len(j)
             if j == x:
-                # print(j)
                 all.append(j)
-    #print(all)
     assert all is not None
 
 
@@ -41,75 +32,48 @@ def test_phone():
     Read_data = []
     all = []
     files = "*.txt"
-    #print(files)
     data = glob.glob(str(files))
     for i in data:
         Read_data.append(open(i).read())
-   # print(Read_data)
     masked_phone = p1.phonenumber(Read_data)
-    #print(masked_phone)
-
     for i in masked_phone:
         wordlist = word_tokenize(i)
-       # print(wordlist)
-       # print(i)
         for j in wordlist:
-            #print(j)
             x = u'\u2588' * len(j)
             if j == x:
-                # print(j)
                 all.append(j)
-    #print(all)
     assert all is not None
 
 def test_date():
     Read_data = []
     all = []
     files = "*.txt"
-    #print(files)
     data = glob.glob(str(files))
     for i in data:
         Read_data.append(open(i).read())
-   # print(Read_data)
     masked_date = p1.dates(Read_data)
-    #print(masked_date)
-
     for i in masked_date:
         wordlist = word_tokenize(i)
-       # print(wordlist)
-       # print(i)
         for j in wordlist:
-            #print(j)
             x = u'\u2588' * len(j)
             if j == x:
-                # print(j)
                 all.append(j)
-    #print(all)
     assert all is not None
 
 def test_concept():
     Read_data = []
     all = []
     files = "*.txt"
-    #print(files)
     data = glob.glob(str(files))
     for i in data:
         Read_data.append(open(i).read())
-   # print(Read_data)
     masked_concept = p1.concept(Read_data,['attention','uneasy'])
-    #print(masked_concept)
-
     for i in masked_concept:
         wordlist = word_tokenize(i)
-       # print(wordlist)
-       # print(i)
         for j in wordlist:
-            #print(j)
             x = u'\u2588' * len(j)
             if j == x:
-                # print(j)
                 all.append(j)
-    #print(all)
     assert all is not None
 
 
@@ -117,38 +81,26 @@ def test_gender():
     Read_data = []
     all = []
     files = "*.txt"
-    #print(files)
     data = glob.glob(str(files))
     for i in data:
         Read_data.append(open(i).read())
-   # print(Read_data)
     masked_gender = p1.gender(Read_data)
-    #print(masked_gender)
-
     for i in masked_gender:
         wordlist = word_tokenize(i)
-       # print(wordlist)
-       # print(i)
         for j in wordlist:
-            #print(j)
             x = u'\u2588' * len(j)
             if j == x:
-                # print(j)
                 all.append(j)
-    #print(all)
     assert all is not None
 
 def test_stats():
     Read_data = []
     all = []
     files = "*.txt"
-    #print(files)
     data = glob.glob(str(files))
     for i in data:
         Read_data.append(open(i).read())
-   # print(Read_data)
     stats = p1.stats(Read_data)
-    #print(stats)
     assert stats is not None
 
 
@@ -156,9 +108,7 @@ def test_output():
     Read_data = []
     all = []
     files = "../output/*.redacted"
-    #print(files)
     data = glob.glob(str(files))
-    #print(data)
     assert data is not None
 
 
